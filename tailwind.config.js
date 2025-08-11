1edf55a3134256129cf05c792a13df4624d2559a
gabrielevaudano98 <gabriele.vaudano@outlook.com>
2025-08-11 23:08:19 +0200

    Reverted all changes back to version 258da29583790445d684683a0194db29a95a3978

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,6 @@ export default {
         sans: ['Inter', 'sans-serif'],
         recoleta: ['Recoleta', 'serif'],
       },
-      keyframes: {
-        'slide-up': {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        'slide-up': 'slide-up 0.3s ease-out forwards',
-      },
     },
   },
   plugins: [],
