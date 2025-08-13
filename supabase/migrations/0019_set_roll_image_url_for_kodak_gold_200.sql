3811ba8350c1427758afe5d9557efc166bf8716e
gabrielevaudano98 <gabriele.vaudano@outlook.com>
2025-08-13 18:43:11 +0200

    [dyad] Confirming dynamic image display for film canisters - wrote 1 file(s), executed 1 SQL queries

---
+++ supabase/migrations/0019_set_roll_image_url_for_kodak_gold_200.sql
@@ -0,0 +1,3 @@
+UPDATE public.film_stocks
+    SET roll_image_url = 'https://ewziosmlinxqywlvegee.supabase.co/storage/v1/object/public/film_rolls/fujifilm/kodak-gold-200.jpg'
+    WHERE name = 'Kodak Gold 200';
