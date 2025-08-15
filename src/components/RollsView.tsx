bf07fbe5522caa1c0297bddd50ac58a3ccf7ae20
gabrielevaudano98 <gabriele.vaudano@outlook.com>
2025-08-15 23:08:05 +0200

    [dyad] Fix syntax error in Rolls view - wrote 1 file(s)

--- src/components/RollsView.tsx
+++ src/components/RollsView.tsx
@@ -8,6 +8,14 @@ import SegmentedControl from './SegmentedControl';
 import CollapsibleAlbumSection from './CollapsibleAlbumSection';
 import RollListItem from './RollListItem';
 
+const DarkroomEmptyState = () => (
+  <div className="text-center py-24 text-neutral-500">
+    <Clock className="w-16 h-16 mx-auto mb-4" />
+    <h3 className="text-xl font-bold text-white">Darkroom is Empty</h3>
+    <p className="mt-2">When you finish a roll, it will appear here to be developed.</p>
+  </div>
+);
+
 const RollsView: React.FC = () => {
   const { completedRolls, albums, refetchRolls, refetchAlbums } = useAppContext();
 
@@ -45,14 +53,6 @@ const RollsView: React.FC = () => {
   const uncategorizedRolls = useMemo(() => {
     return developedRolls.filter(r => !r.album_id);
   }, [developedRolls]);
-
-  const DarkroomEmptyState = () => (
-    <div className="text-center py-24 text-neutral-500">
-      <Clock className="w-16 h-16 mx-auto mb-4" />
-      <h3 className="text-xl font-bold text-white">Darkroom is Empty</h3>
-      <p className="mt-2">When you finish a roll, it will appear here to be developed.</p>
-    </div>
-  );
 
   return (
     <div className="flex flex-col w-full space-y-6">
@@ -119,3 +119,6 @@ const RollsView: React.FC = () => {
       {showCreateAlbumModal && <CreateAlbumModal onClose={() => setShowCreateAlbumModal(false)} />}
     </div>
   );
+};
+
+export default RollsView;
